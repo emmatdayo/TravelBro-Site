cfb87e750eb2f3717bab62329fd2f5683eb9960b
dazy2900 <111284332+emmatdayo@users.noreply.github.com>
2022-11-28 15:36:38 -0800

    update

--- src/component/Dashboard/passenger/SearchTrip.js
+++ src/component/Dashboard/passenger/SearchTrip.js
@@ -77,11 +77,11 @@ const SearchTrips = () => {
       // if (!.length) return <div>No data</div>;
     })
   }
-
+  let price = post.trip_price + '00'
   const config = {
     reference: new Date().getTime().toString(),
     email: cookies.user,
-    amount: 200000,
+    amount: price,
     publicKey: 'pk_test_64126f61df754329ea3017aa6288110785b7cf1d',
     //pk_live_09c77e40e978d48d71865e8a5efd59bd0e089ddb
   }

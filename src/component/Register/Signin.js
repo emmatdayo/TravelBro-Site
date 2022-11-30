30a28d8c837e359df98e925f7a67967efb62bec6
dazy2900 <111284332+emmatdayo@users.noreply.github.com>
2022-11-29 20:43:35 -0800

    Update Signin.js

--- src/component/Register/Signin.js
+++ src/component/Register/Signin.js
@@ -35,12 +35,12 @@ const Signup = () => {
       console.log(response.data)
       //alert(post)
       const status = response.data.status
-      const UserInfo = response.data.user_info[0]
-      const type = response.data.user_info[0].user_status
       console.log(status)
-      console.log(type)
-      console.log(UserInfo)
       if (status == 'logged_in') {
+        const type = response.data.user_info[0].user_status
+        const UserInfo = response.data.user_info[0]
+        console.log(type)
+        console.log(UserInfo)
         window.location.href = '/dashboard'
         setCookie('user', inputs.email, { path: '/' })
         setCookie('page_to_load', type, { path: '/' })

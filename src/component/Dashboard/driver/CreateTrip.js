36d434763c8d71f34c68a0682e3ad991a20b5b1f
dazy2900 <111284332+emmatdayo@users.noreply.github.com>
2022-12-04 09:32:30 -0800

    Update CreateTrip.js

--- src/component/Dashboard/driver/CreateTrip.js
+++ src/component/Dashboard/driver/CreateTrip.js
@@ -106,7 +106,10 @@ const CreateTrip = () => {
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          {post}
+          Woohoo, you're reading this text in a modal!
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close

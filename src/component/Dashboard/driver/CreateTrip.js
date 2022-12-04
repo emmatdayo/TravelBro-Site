5de3e114db8681b4f7df6aa96b20b2c2e1005889
dazy2900 <111284332+emmatdayo@users.noreply.github.com>
2022-12-04 13:59:34 -0800

    Update CreateTrip.js

--- src/component/Dashboard/driver/CreateTrip.js
+++ src/component/Dashboard/driver/CreateTrip.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { Cookies } from 'react-cookie'
 import Modal from 'react-bootstrap/Modal'
 import { Button } from 'bootstrap'
+import React from 'react'
 
 const cookies = new Cookies()
 

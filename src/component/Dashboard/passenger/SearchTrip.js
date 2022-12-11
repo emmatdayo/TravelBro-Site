0676edeb99b74a18b5a46de6e03c25dafa7bf937
dazy2900 <111284332+emmatdayo@users.noreply.github.com>
2022-12-10 16:59:03 -0800

    Update SearchTrip.js

--- src/component/Dashboard/passenger/SearchTrip.js
+++ src/component/Dashboard/passenger/SearchTrip.js
@@ -142,7 +142,7 @@ const SearchTrips = () => {
     const TotalPrice1 = Tprice * seatNumber * 100
     const testTotalPrice = TotalPrice1
     setCookie('price', TotalPrice1, { path: '/' })
-    //setStatePrice(testTotalPrice)
+    setStatePrice(testTotalPrice)
     console.log(TotalPrice1)
     console.log(statePrice)
     console.log(testTotalPrice)
